629fdfb56f3ddd9449b86c5708eda862d88cbe1c
GitHub30 <funaox@gmail.com>
2025-07-25 07:29:49 +0000

    add

--- main.py
+++ main.py
@@ -9,23 +9,22 @@ headers = {
 
 model = None
 
-def load_model():
-    global model
-    if model is None:
-        print('model loading...')
-        model = tf.keras.models.load_model('xserver_captcha.keras')
-    return model
-
 def hello(request):
     if request.method != 'POST':
         return '', 200, headers
     data_url = request.get_data(as_text=True)
+    print(data_url)
     img = tf.io.decode_base64(data_url.split(',')[-1].translate(str.maketrans({'+':'-','/':'_'})))
     img = tf.image.decode_png(img, channels=3)
     img = tf.image.resize(img, [60, 300]) / 255.0
     batch = tf.expand_dims(img, 0)
-    preds = load_model()(batch)
+
+    global model
+    if model is None:
+        model = tf.keras.models.load_model('xserver_captcha.keras')
+    preds = model(batch)
     input_len = tf.fill([tf.shape(preds)[0]], tf.shape(preds)[1])
     decoded = tf.keras.backend.ctc_decode(preds, input_length=input_len, greedy=True)[0][0]
     code = ''.join(str(c) for c in decoded.numpy()[0] if c >= 0)
+    print('iVBOR', code)
     return code, 200, headers
